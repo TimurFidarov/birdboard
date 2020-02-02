c1e077ee538d2bfabc0395b4cd274454f35a8ef2
Classic <nowaybuticanfly@gmail.com>
2020-02-02 15:53:25 +0300

    Added dark theme and created a switcher between themes

--- resources/views/projects/activity/card.blade.php
+++ resources/views/projects/activity/card.blade.php
@@ -1,9 +1,9 @@
-<div class="card mt-3">
+<div class="card mt-3 color:--text-default-color">
     <ul class="list-reset text-xs">
         @foreach($project->activity as $activity)
             <li class=" {{ $loop->last ? '' :  'mb-1' }}">
                 @include("projects.activity.{$activity->description}")
-                <span class="text-grey">{{ $activity->created_at->diffForHumans(false, true) }}</spap>
+                <span class="text-default">{{ $activity->created_at->diffForHumans(false, true) }}</spap>
             </li>
         @endforeach
     </ul>
--- resources/views/projects/form.blade.php
+++ resources/views/projects/form.blade.php
@@ -1,36 +1,38 @@
 @csrf
+
 <div class="field mb-6">
-    <label class="label  mb-2 block" for="title">Title</label>
+    <label class="label text-sm mb-2 block" for="title">Title</label>
 
     <div class="control">
-        <input type="text"
-            class="input bg-transparent border border-grey rounded p-2 text-2xs w-full"
-            name="title"
-            placeholder="I should start ..."
-            value="{{ $project->title }}"
-            required>
+        <input
+                type="text"
+                class="input bg-transparent border border-muted-light rounded p-2 text-xs w-full"
+                name="title"
+                placeholder="My next awesome project"
+                required
+                value="{{ $project->title }}">
     </div>
 </div>
 
-
-<div class="field mb-8">
-    <label class="label  mb-2 block" for="description">Description</label>
+<div class="field mb-6">
+    <label class="label text-sm mb-2 block" for="description">Description</label>
 
     <div class="control">
-        <textarea
-            class="textarea bg-transparent border border-grey rounded p-2 text-2xs w-full"
-            name="description"
-            placeholder="This project is about ..."
-            rows="10"
-            required>{{ $project->description }}</textarea>
+            <textarea
+                name="description"
+                rows="10"
+                class="textarea bg-transparent border border-muted-light rounded p-2 text-xs w-full"
+                placeholder="I should start learning piano."
+                required>{{ $project->description }}</textarea>
     </div>
 </div>
 
 <div class="field">
-    <div class="contorl">
-        <button class="button is-link mr-2" type="submit">{{ $buttonText }}</button>
-        <a href="{{ $project->path() }}">Cancel</a>
+    <div class="control">
+        <button type="submit" class="button is-link mr-2">{{ $buttonText }}</button>
+
+        <a href="{{ $project->path() }}" class="text-default">Cancel</a>
     </div>
 </div>
 
-@include('errors')
+@include ('errors')
